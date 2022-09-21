373d2edf4ae435279f24e15307b2ca971c841296
Duncan Scanga <traceyscanga@MacBook-Pro-II.local>
2022-09-21 12:35:12 -0400

    added documentation to follow pep8

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,37 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-## setting up SQLAlchemy and data models so we can map data models into database tables
+# Setting up SQLAlchemy and data models so we can map data models 
+# into database tables.
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
+# A class to represent a qB&B property.
 class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ## stores the street address (in the format of: 1209 King St W Suite 201)
+
+    # Stores the street address (in the format of: 1209 King St W Suite 201).
     address = db.Column(db.String(120), nullable=False)
-    ## stores the size (currently in square feet)
+
+    # Stores the size of the property (in square feet).
     size = db.Column(db.Integer, nullable=False)
-    ## stores the number of guests the property can house
+
+    # Stores the number of guests the property can house.
     guests = db.Column(db.Integer, nullable=False)
-    ## stores the name of the city
+
+    # Stores the name of the city.
     city = db.Column(db.String(80), nullable=False)
-    ## stores the name of the country
+
+    # Stores the name of the country.
     country = db.Column(db.String(80), nullable=False)
-    ## stores the name of the state/province
+
+    # Stores the name of the state/province.
     state = db.Column(db.String(80), nullable=False)
-    ## stores the postal/zip code
+
+    # Stores the postal/zip code.
     zip_code = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return '<Property %r>' % self.id
-
