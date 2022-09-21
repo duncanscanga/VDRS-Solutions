4e17bdcdda37d6dcff25b46d93c42fcc93240ad9
Duncan Scanga <traceyscanga@MacBook-Pro-II.local>
2022-09-21 12:38:59 -0400

    added more documentation

--- app/__init__.py
+++ app/__init__.py
@@ -8,22 +8,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
-# A class to represent a qB&B property.
 class Property(db.Model):
+    """A class to represent a qB&B property."""
+
     id = db.Column(db.Integer, primary_key=True)
 
-    # Stores the street address (in the format of: 1209 King St W Suite 201).
+    # Stores the street address (format: 1209 King St W Suite 201).
     address = db.Column(db.String(120), nullable=False)
-
+    
     # Stores the size of the property (in square feet).
     size = db.Column(db.Integer, nullable=False)
-
+    
     # Stores the number of guests the property can house.
     guests = db.Column(db.Integer, nullable=False)
-
+    
     # Stores the name of the city.
     city = db.Column(db.String(80), nullable=False)
-
+    
     # Stores the name of the country.
     country = db.Column(db.String(80), nullable=False)
 
