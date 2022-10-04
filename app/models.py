9bb6f9ec667173734546324b05b69503cc47bee5
Duncan Scanga <duncanscanga@gmail.com>
2022-10-04 18:46:31 -0400

    fixing testing error

--- app/models.py
+++ app/models.py
@@ -128,6 +128,10 @@ class Review(db.Model):
         return "<Review %r>" % self.id
 
 
+# create all tables
+db.create_all()
+
+
 def register(name, email, real_name, password, billing_address, postal_code):
     '''
     Register a new user
@@ -340,7 +344,3 @@ def owner_check(owner_id):
     if len(existed) > 0:
         return True
     return False
-
-
-# create all tables
-db.create_all()
