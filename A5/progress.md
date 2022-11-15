1e017842add45685c1541a98dec9ff5baa8a968a
SamEThibault <sam5thibault@gmail.com>
2022-11-14 22:15:17 -0500

    docker configurations added

---
+++ A5/progress.md
@@ -0,0 +1,27 @@
+# Duncan:
+
+Dev Branch:
+Progress:
+Difficulties:
+Plan:
+
+# Rahul:
+
+Dev Branch:
+Progress:
+Difficulties:
+Plan:
+
+# Vasuki:
+
+Dev Branch:
+Progress:
+Difficulties:
+Plan:
+
+# Sam:
+
+Dev Branch:
+Progress:
+Difficulties:
+Plan:
--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,13 @@ templates = os.path.join(
 )
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
+
+db_string = os.getenv('db_string')
+if db_string:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_string
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 
